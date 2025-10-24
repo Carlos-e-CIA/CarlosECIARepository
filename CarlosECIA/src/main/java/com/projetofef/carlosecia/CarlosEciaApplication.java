a00992b37db1374e07e8a551ce1a610631938388
eduardozaparoli <eduardozaparoli10@yahoo.com>
2025-10-23 21:29:38 -0300

    Criação do Projeto CarlosECIA

---
+++ CarlosECIA/src/main/java/com/projetofef/carlosecia/CarlosEciaApplication.java
@@ -0,0 +1,13 @@
+package com.projetofef.carlosecia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarlosEciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarlosEciaApplication.class, args);
+    }
+
+}
---
+++ CarlosECIA/src/test/java/com/projetofef/carlosecia/CarlosEciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetofef.carlosecia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarlosEciaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
